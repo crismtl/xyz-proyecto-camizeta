ace90273e2db79d1210ad323201fc033af0ca56a
Cristian Toaquiza <crismtl@gmail.com>
2016-09-30 06:32:55 -0500

    upload error

---
+++ api/controllers/AvatarController.js
@@ -0,0 +1,24 @@
+/**
+ * AvatarController
+ *
+ * @description :: Server-side logic for managing Avatars
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    upload: function(req, res) {
+        var parametros = req.allParams();
+        console.log('parametros', parametros);
+
+        req.file('file')
+            .upload({
+                maxBytes: 1000000
+            }, function whenDone(err, uploadedFiles) {
+                if (err) return res.serverError(err);
+                else return res.json({
+                    files: uploadedFiles,
+                    textParams: req.params.all()
+                });
+            });
+    }
+};
